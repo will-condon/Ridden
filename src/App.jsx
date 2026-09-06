5e1c53201dc29eee97c8dfc31149f1395ea19b9e
will-condon <willcondon04@gmail.com>
2026-09-06 16:28:37 +1200

    testing components

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Chip from './components/Chip.jsx';
+import PrimaryButton from './PrimaryButton.jsx';
 
 function App() {
   return (
     <div className="app">
       <h1>Ridden</h1>
       <Chip label="Hazard" />
+      <PrimaryButton label="Submit"></PrimaryButton>
     </div>
   );
 }
